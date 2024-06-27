576d176d6b2355ab6ba062a3bf0b5791fa8409ae
Yersa Nordman <finwo@pm.me>
2024-06-27 22:40:49 +0200

    Removed unused code comments from kvsmctl

--- util/kvsmctl.c
+++ util/kvsmctl.c
@@ -230,33 +230,5 @@ int main(int argc, char **argv) {
 
   kvsm_close(ctx);
 
-  /*struct kvsm *ctx = kvsm_open("pizza.db", 0);*/
-  /*struct kvsm_transaction *tx = kvsm_transaction_init(ctx);*/
-
-  /*kvsm_transaction_set(tx,*/
-  /*    &((struct buf){ .data = "foo", .len = 3 }),*/
-  /*    &((struct buf){ .data = "bar", .len = 3 })*/
-  /*);*/
-
-  /*kvsm_transaction_commit(tx);*/
-  /*kvsm_transaction_free(tx);*/
-
-  /*kvsm_close(ctx);*/
-
-
-
-  /* log_info("Writing mock data: foo = bar..."); */
-  /* tx = kvsm_transaction_init(); */
-  /* kvsm_transaction_set(tx, &((struct buf){ .data = "foo", .len = 3 }), &((struct buf){ .data = "bar", .len = 3 })); */
-  /* kvsm_transaction_store(kvsm_state, tx); */
-  /* kvsm_transaction_free(tx); */
-  /* log_info("OK\n"); */
-
-  /* log_info("Reading mock data: foo = bar..."); */
-  /* tx = kvsm_transaction_init(); */
-  /* struct buf *received = kvsm_transaction_get(tx, &((struct buf){ .data = "xxx", .len = 3 })); */
-  /* kvsm_transaction_free(tx); */
-  /* log_info("OK\n"); */
-
   return 0;
 }
