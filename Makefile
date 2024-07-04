594d6849e6ad12d8d453c5db94c6d0ac591c461c
Yersa Nordman <finwo@pm.me>
2024-07-04 16:19:44 +0200

    Added basic testing workflow

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SRC:=
 INCLUDES?=-Isrc
 SRC+=$(wildcard src/*.c)
+CC?=clang
 
 override CFLAGS?=-Os -Wall
 
@@ -13,13 +14,16 @@ OBJ:=$(SRC:.c=.o)
 OBJ:=$(OBJ:.cc=.o)
 
 .PHONY: all
-all: kvsmctl README.md
+all: kvsmctl test README.md
 
 .c.o:
 	${CC} ${INCLUDES} ${CFLAGS} -c $< -o $@
 
 kvsmctl: util/kvsmctl.c $(OBJ)
 	${CC} ${INCLUDES} ${CFLAGS} ${LDFLAGS} ${OBJ} $< -o $@
+
+test: ${OBJ} test.c
+	${CC} ${INCLUDES} ${CFLAGS} ${LDFLAGS} ${OBJ} $@.c -o $@
 
 README.md: src/kvsm.h
 	stddoc < $< > README.md
---
+++ test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "finwo/assert.h"
+#include "rxi/log.h"
+
+#include "src/kvsm.h"
+
+void test_kvsm_regular() {
+  struct kvsm *ctx;
+
+  ctx = kvsm_open("test.db", 0);
+  ASSERT("Opening a file returns a context", ctx != NULL);
+  ASSERT("Closing a file context returns OK", kvsm_close(ctx) == KVSM_OK);
+
+  ctx = kvsm_open(NULL, 0);
+  ASSERT("Opening a NULL returns no context", ctx == NULL);
+  ASSERT("Closing a NULL context returns ERROR", kvsm_close(ctx) != KVSM_OK);
+
+}
+
+int main() {
+
+  // Seed random
+  unsigned int seed;
+  FILE* urandom = fopen("/dev/urandom", "r");
+  fread(&seed, sizeof(int), 1, urandom);
+  fclose(urandom);
+  srand(seed);
+
+  // No verbose logging here
+  log_set_level(LOG_FATAL);
+
+  // Run the actual tests
+  RUN(test_kvsm_regular);
+  /*RUN(test_mindex_structs);*/
+  return TEST_REPORT();
+}
